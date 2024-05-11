e4f09a174e51d7db03fdd8180e8f5da19dc2fe42
RanjithKumarT35 <51657707+RanjithKumarT35@users.noreply.github.com>
2024-05-11 16:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({ origin: [`process.env.frontUrl`], credentials: true }));
+app.use(cors({ origin: "https://blog.revnitro.com", credentials: true }));
 // connect to DB
 try {
   mongoose.connect(process.env.mongoUrl);
